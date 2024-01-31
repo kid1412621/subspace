f5647e184cd5f30ac3beb71cf71f4de385671638
Alex <kid1412621@gmail.com>
2024-01-31 22:20:40 +0800

    WIP

--- app/src/main/kotlin/me/nanova/subspace/ui/Settings.kt
+++ app/src/main/kotlin/me/nanova/subspace/ui/Settings.kt
@@ -1,9 +1,43 @@
 package me.nanova.subspace.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Settings() {
     Text("Under construction...")
+
+    var host by rememberSaveable { mutableStateOf("") }
+    var user by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        TextField(
+            value = host,
+            onValueChange = { host = it },
+            label = { Text("Host") },
+            singleLine = true,
+            placeholder = { Text("http://host") }
+        )
+
+        TextField(
+            value = user,
+            onValueChange = { user = it },
+            label = { Text("User") },
+            singleLine = true,
+            placeholder = { Text("Your username") }
+        )
+    }
 }
